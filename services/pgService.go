dd82874269f156ae2feaf2dbc55809b7be88d419
amine-amaach <amine.amaach@edu.uca.ma>
2022-07-08 21:53:08 +0100

    Add pgService implementing pgPort interface

---
+++ services/pgService.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/amine-amaach/simulators/pgmqtt/services/models"
+)
+
+// PgmqttService describes a service that generates power-generator data to
+// be published to an MQTT Broker.
+type PgmqttService interface {
+
+	// BuildPowerGenerators returns a slice of power-generators of length nb.
+	BuildPowerGenerators(ctx context.Context, nb int) ([]models.Generator, error)
+
+	// MakeMessagePayload generates random data for a given power-generator and
+	// returns a map contains its topics with corresponding message payloads.
+	MakeMessagePayloads(ctx context.Context, pg models.Generator) (map[string]json.RawMessage, error)
+}
